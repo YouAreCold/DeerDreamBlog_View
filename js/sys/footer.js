915f97dc6926c56eaddb7a70524bcafd2cf9ad7d
佳鑫 吴 <747903314@qq.com>
2018-12-25 15:55:52 +0800

    以鹿为梦博客页面-首次成型v1.0

---
+++ js/sys/footer.js
@@ -0,0 +1,103 @@
+var footerString = '<div class="container">                                                                                           '+
+					'<div class="row row-pb-md">                                                                                      '+
+					'	<div class="col-md-3">                                                                                        '+
+					'		<h2>导航栏</h2>                                                                                            '+
+					'		<p>                                                                                                       '+
+					'			<ul class="colorlib-footer-links">                                                                    '+
+					'				<li><a href="javascript:void();"><i class="icon-reply"></i> 首页</a></li>                         '+
+					'				<li><a href="javascript:void();"><i class="icon-reply"></i> 吐槽</a></li>                         '+
+					'				<li><a href="javascript:void();"><i class="icon-reply"></i> 归纳</a></li>                         '+
+					'				<li><a href="javascript:void();"><i class="icon-reply"></i> 友链</a></li>                         '+
+					'				<li><a href="javascript:void();"><i class="icon-reply"></i> 关于</a></li>                         '+
+					'				<li><a href="javascript:void();"><i class="icon-reply"></i> 搜索 🔎</a></li>                       '+
+					'			</ul>                                                                                                 '+
+					'		</p>                                                                                                      '+
+					'	</div>                                                                                                        '+
+					'	<div class="col-md-3">                                                                                        '+
+					'		<h2>友链</h2>                                                                                             '+
+					'		<div class="f-blog">                                                                                      '+
+					'			<a href="http://www.github.com" class="blog-img" style="background-image: url(../images/sys/github-footer.png);">   '+
+					'			</a>                                                                                                  '+
+					'			<div class="desc">                                                                                    '+
+					'				<h3><a href="http://www.github.com">GitHub</a></h3>                                               '+
+					'				<p class="admin"><span>Built for developers</span></p>                                            '+
+					'			</div>                                                                                                '+
+					'		</div>                                                                                                    '+
+					'		<div class="f-blog">                                                                                      '+
+					'			<a href="http://www.baidu.com" class="blog-img" style="background-image: url(../images/sys/baidu-footer.png);">    '+
+					'			</a>'+
+					'			<div class="desc">'+
+					'				<h3><a href="http://www.baidu.com">百度</a></h3>'+
+					'				<p class="admin"><span>百度一下，你就知道</span></p>'+
+					'			</div>'+
+					'		</div>'+
+					'		<div class="f-blog">'+
+					'			<a href="https://translate.google.cn" class="blog-img" style="background-image: url(../images/sys/google-footer.png);">'+
+					'			</a>'+
+					'			<div class="desc">'+
+					'				<h3><a href="https://translate.google.cn">谷歌翻译</a></h3>       '+
+					'				<p class="admin"><span>超好用的翻译 T.T</span></p>                '+
+					'			</div>'+
+					'		</div>'+
+					'	</div>'+
+					'	<div class="col-md-3">'+
+					'		<h2>博主的资料</h2>'+
+					'		<p>'+
+					'			<ul class="colorlib-footer-links">'+
+					'				<li><a href="javascript:void();"><i class="icon-paperplane"></i>邮箱：747903314@qq.com</a></li>'+
+					'				<li><a href="javascript:void();"><i class="icon-phone3"></i>手机：保密 T.T</a></li>'+
+					'				<li><a href="javascript:void();"><i class="icon-globe"></i>GitHub：747903314@qq.com</a></li>'+
+					'			</ul>                                                                     '+
+					'		</p>                                                                          '+
+					'		<h2>博主推荐语言</h2>                                                          '+
+					'		<p>                                                                           '+
+					'			<ul class="colorlib-footer-links">                                        '+
+					'				<li><a href="javascript:void();"><i class="icon-location-2"></i> JAVA</a></li>         '+
+					'				<li><a href="javascript:void();"><i class="icon-location-2"></i> ASP.NET</a></li>      '+
+					'				<li><a href="javascript:void();"><i class="icon-location-2"></i> HTML、JS</a></li>     '+
+					'			</ul>                                                                     '+
+					'		</p>                                                                          '+
+					'	</div>                                                                            '+
+					'	<div class="col-md-3">                                                          '+
+					'		<h2>标签</h2>                                                               '+
+					'		<p class="tags">                                                            '+
+					'			<span><a href="#"><i class="icon-tag"></i> JAVA</a></span>               '+
+					'			<span><a href="#"><i class="icon-tag"></i> C#</a></span>                 '+
+					'			<span><a href="#"><i class="icon-tag"></i> AJ</a></span>                 '+
+					'			<span><a href="#"><i class="icon-tag"></i> 2K19</a></span>               '+
+					'			<span><a href="#"><i class="icon-tag"></i> 吃鸡</a></span>               '+
+					'			<span><a href="#"><i class="icon-tag"></i> 声控</a></span>               '+
+					'			<span><a href="#"><i class="icon-tag"></i> Travel</a></span>             '+
+					'			<span><a href="#"><i class="icon-tag"></i> Exercise</a></span>           '+
+					'			<span><a href="#"><i class="icon-tag"></i> Health</a></span>             '+
+					'			<span><a href="#"><i class="icon-tag"></i> News</a></span>               '+
+					'			<span><a href="#"><i class="icon-tag"></i> Model</a></span>              '+
+					'			<span><a href="#"><i class="icon-tag"></i> Women</a></span>              '+
+					'			<span><a href="#"><i class="icon-tag"></i> Animals</a></span>            '+
+					'			<span><a href="#"><i class="icon-tag"></i> Nature</a></span>             '+
+					'			<span><a href="#"><i class="icon-tag"></i> Art</a></span>                '+
+					'			<span><a href="#"><i class="icon-tag"></i> Sea</a></span>               '+
+					'			<span><a href="#"><i class="icon-tag"></i> Ocean</a></span>             '+
+					'			<span><a href="#"><i class="icon-tag"></i> Office</a></span>            '+
+					'			<span><a href="#"><i class="icon-tag"></i> Home</a></span>              '+
+					'		</p>'+
+					'	</div>'+
+					'</div>'+
+					'<div class="row" style="text-align:center">'+
+					'	<div class="col-md-12">'+
+					'		<p>'+
+					'			<small class="block">'+
+					'				Copyright &copy;'+
+					'				<a href="javascript:void();" target="_blank" title="以鹿为梦"> 以鹿为梦</a>'+
+					'				<script>'+
+					'					document.write(new Date().getFullYear());'+
+					'				</script>'+
+					'				粤ICP备XXXXX号-1 | 粤公网安备99999999999999号 <i class="icon-heart" aria-hidden="true"></i>'+
+					'				<a href="https://github.com/wjxliyan" rel="nofollow" target="_blank" title="GitHub"> Copyright © wujiaxin.cn'+
+					'					<i class="fa fa-github" aria-hidden="true"></i></a>'+
+					'			</small>'+
+					'		</p>'+
+					'	</div>'+
+					'</div>'+
+				'</div>';
+document.getElementById('colorlib-footer').innerHTML=footerString;
---
+++ js/sys/head.js
@@ -0,0 +1,37 @@
+var headString = '<div class="top-menu">'+
+				'<div class="container">'+
+					'<div class="row">'+
+						'<div class="col-xs-2">'+
+						'	<div id="colorlib-logo"><a href="index.html"><img src="../images/sys/logo-title.png"/></a></div>'+
+						'</div>'+
+						'<div class="col-xs-10 text-right menu-1">'+
+							'<ul>'+
+								'<li><a href="index.html">首页</a></li> <!-- class="active" -->'+
+								'<li><a href="makeComplaints.html">吐槽</a></li>'+
+								'<li class="dropdown">'+
+										'<a href="statistics.html">'+
+										'		归纳'+
+										'</a>'+
+										'<ul class="dropdown-menu">'+
+												'<li><a href="travel-welfare.html">经验福利</a></li>'+
+												'<li><a href="travel-skill.html">建站技巧</a></li>'+
+												'<li><a href="travel-SourceSharing.html">源码分享</a></li>'+
+										'</ul>'+
+								'</li>'+
+								'<li><a href="links.html">友链</a></li>'+
+								'<li class="dropdown">'+
+										'<a href="about.html">'+
+										'		关于'+
+										'</a>'+
+										'<ul class="dropdown-menu">'+
+												'<li><a href="readerWall.html">读者墙</a></li>'+
+										'</ul>'+
+								'</li>'+
+								'<li><a href="search.html">搜索 🔎</a></li>'+
+							'</ul>'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+			'</div>';
+document.getElementById('nav-head').innerHTML=headString;
+
